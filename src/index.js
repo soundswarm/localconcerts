9a238f5560a662fabffad5d004b04790190f4274
Sean Connor <seantconnor@gmail.com>
2017-08-13 12:17:58 -0700

    redirect in place

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Callback from './Callback'
+import Home from './Home'
 import registerServiceWorker from './registerServiceWorker';
-import {Router, Route, browserHistory} from 'react-router';
-import createBrowserHistory from 'history/createBrowserHistory'
-const history = createBrowserHistory()
+import {Router, Route, Redirect} from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+const history = createBrowserHistory();
 ReactDOM.render(
   <Router history={history}>
+  <div>
+    <Route exact path="/" component={App}/>
 
-    <Route path="/" component={App} />
+    <Route path="/home" component={Home}/>
+    <Route path="/callback" component={Callback}/>
+    </div>
   </Router>,
   document.getElementById('root'),
 );
